36e1a46831f03f34e2537866b5e691c258a8696e
bilal-ch <bilalchaudhary786@yahoo.com>
2016-06-23 18:33:21 -0500

    created show page and route

--- app/controllers/fallen_heroes_controller.rb
+++ app/controllers/fallen_heroes_controller.rb
@@ -1,2 +1,8 @@
 class FallenHeroesController < ApplicationController
+
+  def show
+    @fallen_hero = FallenHero.find(params[:id])
+    render 'show.html.erb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/fallen_heroes/:id' => 'fallen_heroes#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
