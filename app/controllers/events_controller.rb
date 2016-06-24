2a2c46d7e4447589688453a4b2ba4f8deadf3638
AnnaGulstine <annagulstine@gmail.com>
2016-06-24 15:08:46 -0500

    Added show action to Events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,9 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
 end
