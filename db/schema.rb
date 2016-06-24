6b798435003d8be752c11c216836459908fce374
bilal-ch <bilalchaudhary786@yahoo.com>
2016-06-23 20:50:53 -0500

    did rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621011542) do
+ActiveRecord::Schema.define(version: 20160624014314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "image"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "datetime"
+    t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "fallen_heros", force: :cascade do |t|
     t.string   "rank"
