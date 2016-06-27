01134abb8d449da1b5bc50103b3389a65e60cae0
bilal-ch <bilalchaudhary786@yahoo.com>
2016-06-27 18:37:46 -0500

    added destroy action to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,11 @@
 class EventsController < ApplicationController
+  def destroy
+    event = Event.find_by(id: params[:id])
+
+    if event.destroy
+      redirect_to '/events'
+    else 
+      redirect_to "/events/#{event.id}"
+    end
+  end
 end
