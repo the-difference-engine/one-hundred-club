153ccbf173d32501520d317ddebdd61640fb4e01
Tommaso Bufano <tbufano@thinkseed.com>
2016-06-27 19:30:43 -0500

    Made a new and create controller for events and added a blank new show page.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,22 @@ class EventsController < ApplicationController
     @event = Event.find_by(id: params[:id])
     render 'show.html.erb'
   end
+
+  def new
+  end
+
+  def create
+    @event = Event.create(
+      image: params[:image],
+      title: params[:title],
+      description: params[:description],
+      datetime: params[:datetime],
+      location: params[:location]
+    )
+    if @event.save
+      redirect_to "/events/#{@event.id}"
+    else
+      render "new.html.erb"
+    end
+  end
 end
