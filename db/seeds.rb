a950aab27c4fbe56e7faee8dade326fa1ab448b0
Nikki Veen <nikkiveen@gmail.com>
2016-06-27 21:08:49 -0500

    added member seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,33 @@
 50.times do
   FallenHero.create(rank: ["Officer", "Firefighter", "Investigator"].sample, first_name: Faker::Name.first_name, middle_name: Faker::Name.first_name, last_name: Faker::Name.last_name, department: ["Chicago Police", "Chicago Fire Department", "Cook County Sheriff's Department"].sample, date_deceased: Faker::Date.backward(14), description: Faker::Hipster.paragraphs(1)[0], image_url: Faker::Placeholdit.image("50x50"))
 end
+
+50.times do
+  Member.create(
+    title: Faker::Name.title,
+    first_name: Faker::Name.first_name,
+    middle_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    suffix: Faker::Name.suffix,
+    company: Faker::Company.name,
+    job_title: Faker::Company.profession,
+    name_on_card: Faker::Name.name,
+    other_name_1: Faker::Name.name,
+    other_name_2: Faker::Name.name,
+    other_name_3: Faker::Name.name,
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip_code: Faker::Address.zip,
+    country: 'United States',
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number,
+    how_they_heard: ['Advertising',
+                     'Event Initiative',
+                     'Newsletter/Print Media',
+                     'Personal Referral',
+                     'Website'
+                     ].sample,
+    if_other: params[:if_other]
+  )
+end
