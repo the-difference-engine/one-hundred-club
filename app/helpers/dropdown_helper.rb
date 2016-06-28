eddf5b1b6bee16b1e440869faaf1efd3476a0542
Nikki Veen <nikkiveen@gmail.com>
2016-06-27 20:17:58 -0500

    added helper file for form dropdowns

---
+++ app/helpers/dropdown_helper.rb
@@ -0,0 +1,74 @@
+module DropdownHelper
+  def us_states
+    [
+      ['AK', 'AK'],
+      ['AL', 'AL'],
+      ['AR', 'AR'],
+      ['AZ', 'AZ'],
+      ['CA', 'CA'],
+      ['CO', 'CO'],
+      ['CT', 'CT'],
+      ['DC', 'DC'],
+      ['DE', 'DE'],
+      ['FL', 'FL'],
+      ['GA', 'GA'],
+      ['HI', 'HI'],
+      ['IA', 'IA'],
+      ['ID', 'ID'],
+      ['IL', 'IL'],
+      ['IN', 'IN'],
+      ['KS', 'KS'],
+      ['KY', 'KY'],
+      ['LA', 'LA'],
+      ['MA', 'MA'],
+      ['MD', 'MD'],
+      ['ME', 'ME'],
+      ['MI', 'MI'],
+      ['MN', 'MN'],
+      ['MO', 'MO'],
+      ['MS', 'MS'],
+      ['MT', 'MT'],
+      ['NC', 'NC'],
+      ['ND', 'ND'],
+      ['NE', 'NE'],
+      ['NH', 'NH'],
+      ['NJ', 'NJ'],
+      ['NM', 'NM'],
+      ['NV', 'NV'],
+      ['NY', 'NY'],
+      ['OH', 'OH'],
+      ['OK', 'OK'],
+      ['OR', 'OR'],
+      ['PA', 'PA'],
+      ['RI', 'RI'],
+      ['SC', 'SC'],
+      ['SD', 'SD'],
+      ['TN', 'TN'],
+      ['TX', 'TX'],
+      ['UT', 'UT'],
+      ['VA', 'VA'],
+      ['VT', 'VT'],
+      ['WA', 'WA'],
+      ['WI', 'WI'],
+      ['WV', 'WV'],
+      ['WY', 'WY']
+    ]
+  end
+
+  def countries
+    [
+      ['United States', 'United States']
+    ]
+  end
+
+  def how_did_you_hear
+    [
+      ['Advertising', 'Advertising'],
+      ['Event Initiative', 'Event Initiative'],
+      ['Newsletter/Print Media', 'Newsletter/Print Media'], 
+      ['Personal Referral', 'Personal Referral'],
+      ['Website', 'Website'],
+      ['Other', 'Other']
+    ]
+  end
+end
