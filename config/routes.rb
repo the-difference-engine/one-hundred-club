e708fc27de30a34d045f331f64e3b4492cc1c4d5
Jay Wengrow <jaywngrw@gmail.com>
2016-06-30 20:56:36 -0500

    installed Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'fallen_heroes#index'
   get '/fallen_heroes' => 'fallen_heroes#index'
   get '/fallen_heroes/new' => 'fallen_heroes#new'
   post '/fallen_heroes' => 'fallen_heroes#create'
