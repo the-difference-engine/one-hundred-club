a33cb519629993d2ddb1977a2739a3026787eb1f
AnnaGulstine <annagulstine@gmail.com>
2016-07-09 13:37:03 -0500

    added create in routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   get '/members/:id' => 'members#show'
   ##########################################################
   get '/donations/new' => 'donations#new'
+  post '/donations' => 'donations#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
