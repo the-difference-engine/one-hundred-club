f7e0b19171d3a00ef9d4f9e28cf70825a5bc5ea1
AnnaGulstine <annagulstine@gmail.com>
2016-07-11 17:01:42 -0500

    commented out line about using ActiveRecord fixtures

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Donation, type: :model do
   it "has a valid factory" do
     expect(FactoryGirl.build(:donation)).to be_valid
   end
+
+  it "is invalid without a first_name" do 
+    donation = FactoryGirl.build(:donation, first_name: nil)
+    donation.valid?
+    expect(donation.errors[:first_name]).to include("can't be blank")
+  end
 end
