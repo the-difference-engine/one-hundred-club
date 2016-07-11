a5d479032b8e3bcb38ba4510b41b17f524b12841
Nikki Veen <nikkiveen@gmail.com>
2016-07-11 18:58:36 -0500

    added carrierwave s3 configurations

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,13 +2,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'xxx',                        # required
-    aws_secret_access_key: 'yyy',                        # required
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    aws_access_key_id:     ENV[S3_ACCESS_KEY_ID],                        # required
+    aws_secret_access_key: ENV[S3_SECRET_ACCESS_KEY_ID],                        # required
+    region:                'us-east-1'               # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'name_of_directory'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  config.fog_directory  = 'one-hundred-club-chicago'                              # optional, defaults to true
+#   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
