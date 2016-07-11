abee53144664b9421a808cb697ef09956854b442
AnnaGulstine <annagulstine@gmail.com>
2016-07-10 19:00:23 -0500

    added controller tests for #new

--- spec/controllers/donations_controller_spec.rb
+++ spec/controllers/donations_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe DonationsController, type: :controller do
-
+  describe 'GET #new' do
+    it "assigns a new Donation to @donation" do
+      get :new
+      expect(assigns(:donation)).to be_a_new(Donation)
+    end
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
 end
