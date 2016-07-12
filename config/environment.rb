88e6a848a3a07e443faf812474e507c0b5a3c212
AnnaGulstine <annagulstine@gmail.com>
2016-07-11 19:40:38 -0500

    added sendgrid to username and password

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ Rails.application.initialize!
 
 # Specify ActionMailer settings to point to SendGrid's servers
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['USERNAME'],
-  :password => ENV['PASSWORD'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'yourdomain.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
