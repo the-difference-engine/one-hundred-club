e6bbff81a257ba24cfcccee42a6d35cea71b6cda
bilal-ch <bilalchaudhary786@yahoo.com>
2016-07-14 20:50:02 -0500

    recreated member table

---
+++ db/migrate/20160715011835_recreated_member_table.rb
@@ -0,0 +1,28 @@
+class RecreatedMemberTable < ActiveRecord::Migration
+  def change
+    create_table :members do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :title
+      t.string :middle_name
+      t.string :suffix
+      t.string :company
+      t.string :job_title
+      t.string :name_on_card
+      t.string :other_name_1
+      t.string :other_name_2
+      t.string :other_name_3
+      t.string :country
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :email
+      t.integer :phone
+      t.string :how_they_heard
+      t.string :if_other
+      t.integer :zip_code
+
+      t.timestamps null: false
+    end
+  end
+end
