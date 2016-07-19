fc0c1712a25a8ae23e9145a5baa26ee78f0773ea
Nikki Veen <nikkiveen@gmail.com>
2016-07-18 20:51:52 -0500

    updated the edit form and the update action for user permisisons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,16 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find_by(id: params[:id])
+    user = User.find_by(id: params[:id])
+    user.update(
+      user_access: params[user_access],
+      blog_access: params[blog_access],
+      events_access: params[events_access],
+      fallen_hero_access: params[fallen_hero_access],
+      member_access: params[member_access],
+      donation_access: params[donation_access],
+      reporting_access: params[user_access]
+    )
     redirect_to '/users/#{@user.id}'
   end
 end
