67972e1587dd0aa869878cb0e35b6644b6ed44db
Nikki Veen <nikkiveen@gmail.com>
2016-07-18 19:58:58 -0500

    created user show page

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   get 'donations/:id' => 'donations#show'
   ##########################################################
   get '/users' => 'users#index'
+  get '/users/:id' => 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
