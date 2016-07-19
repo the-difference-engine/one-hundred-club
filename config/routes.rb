59998d3ded30a9ca349143440dde2e037ac3557c
Nikki Veen <nikkiveen@gmail.com>
2016-07-18 19:44:14 -0500

    created user index route

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
   get '/donations/new' => 'donations#new'
   post '/donations' => 'donations#create'
   get 'donations/:id' => 'donations#show'
+  ##########################################################
+  get '/users' => 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
