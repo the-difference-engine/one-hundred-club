c5cfd5719c7bedac022ac04906842d93d27a35f4
Nikki Veen <nikkiveen@gmail.com>
2016-07-18 20:41:30 -0500

    added user edit and update routes and began building out edit view

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,8 @@ Rails.application.routes.draw do
   ##########################################################
   get '/users' => 'users#index'
   get '/users/:id' => 'users#show'
-
+  get '/users/:id/edit' => 'users#edit'
+  patch '/users/:id' => 'users#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
