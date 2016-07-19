6e9d3ec9623235e812a9bf79fe7b971d32c0b37d
AnnaGulstine <annagulstine@gmail.com>
2016-07-18 19:57:25 -0500

    added update action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -45,6 +45,30 @@ class MembersController < ApplicationController
 
   def edit
     @member = Member.find_by(id: params[:id])
-    # puts current_user.id
+  end
+
+  def update
+    member = Member.find_by(id: params[:id])
+    member.update(
+      title: params[:title],
+      first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      last_name: params[:last_name],
+      suffix: params[:suffix],
+      company: params[:company],
+      job_title: params[:job_title],
+      name_on_card: params[:name_on_card],
+      other_name_1: params[:other_name_1],
+      other_name_2: params[:other_name_2],
+      other_name_3: params[:other_name_3],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip_code: params[:zip_code],
+      country: params[:country],
+      email: params[:email],
+      phone: params[:phone]
+    )
+    redirect_to "/members/#{member.id}"
   end
 end
