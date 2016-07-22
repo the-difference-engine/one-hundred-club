f4c906a06b2fdf547047bc38068f5a8df7e5b1ef
Tommaso Bufano <tbufano@thinkseed.com>
2016-07-21 20:13:11 -0500

    Added blog_posts routes and controller methods.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,2 +1,14 @@
 class BlogPostsController < ApplicationController
+  def create
+    @blog_post = BlogPost.create(
+      title: params[:title],
+      content: params[:content],
+      image: params[:image]
+    )
+    if @blog_post.save
+      redirect_to "/blog_posts/#{@blog_post.id}"
+    else
+      render "new.html.erb"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -35,8 +35,9 @@ Rails.application.routes.draw do
   get '/users/:id/edit' => 'users#edit'
   patch '/users/:id' => 'users#update'
   ##########################################################
-
-
+  post '/blog_posts' => 'blog_posts#create'
+  get '/blog_posts/:id' => 'blog_posts#show'
+  ##########################################################
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
