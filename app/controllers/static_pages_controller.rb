c3b413e4e693aca710012fed453e8ac209b744b0
Nikki Veen <nikkiveen@gmail.com>
2016-08-12 23:34:43 -0500

    added some text to html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     # news - last 2 by created date
-    
+    @recent_news = BlogPost.last(2)
     # events - date >= today - limit 4
-
+    @future_events = Event.where("datetime >= ?", Date.today).first(4)
     render 'home.html.erb'
   end
 end
