cbff857aba2955ecea1431047a7afb6b8dd121a8
Nikki Veen <nikkiveen@gmail.com>
2016-08-12 20:41:10 -0500

    added home route to nav bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    # news - last 2 by created date
+    
+    # events - date >= today - limit 4
+
     render 'home.html.erb'
   end
 end
