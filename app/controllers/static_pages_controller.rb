69f4c5e2b1c1c0d000eb9da93a26e3130a59517f
Nikki Veen <nikkiveen@gmail.com>
2016-08-18 14:35:02 -0500

    added wells for news and events

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     # news - last 2 by created date
     @recent_news = BlogPost.last(2)
     # events - date >= today - limit 4
-    @future_events = Event.where("datetime >= ?", Date.today).first(4)
+    @future_events = Event.where('datetime >= ?', Date.today).first(4)
     render 'home.html.erb'
   end
 end
