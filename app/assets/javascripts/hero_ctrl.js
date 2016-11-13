1c0bd696ab0c990e475c4f35232786d201fd54c2
AriSendic <arimsendic@gmail.com>
2016-11-13 11:33:27 -0600

    before adding ngclicks

--- app/assets/javascripts/hero_ctrl.js
+++ app/assets/javascripts/hero_ctrl.js
@@ -2,7 +2,7 @@
   angular.module("app").controller("heroCtrl", function($scope, $http) {
     $scope.setup = function() {
       $http.get('/api/fallen_heroes.json').then(function(response) {
-        $scope.fallen_heroes = response.data;
+        $scope.fallenHeroes = response.data;
       });
     };
   });
