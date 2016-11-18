c01bfd5d68011a5d85bd432ff1c9862e162667ed
AriSendic <arimsendic@gmail.com>
2016-11-17 19:05:49 -0600

    fixed formating in heroctrl- was overwriting app

--- app/assets/javascripts/hero_ctrl.js
+++ app/assets/javascripts/hero_ctrl.js
@@ -1,5 +1,10 @@
-var app = angular.module('app',[]);
-app.controller('heroCtrl',function($scope) {
+(function() {
+  "use strict";
+
+  angular.module("app").controller("heroCtrl", function($scope) {
+
+
   $scope.show = 1;
 
 });
+})();
