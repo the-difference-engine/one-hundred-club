e5c6c5a11ed36976ad54eaa9511bd0db1d9314dc
David Basaraba <d.basaraba@gmail.com>
2016-11-28 20:52:03 -0600

    More work on angular

--- app/assets/javascripts/hero_ctrl.js
+++ app/assets/javascripts/hero_ctrl.js
@@ -3,8 +3,62 @@
 
   angular.module("app").controller("heroCtrl", function($scope) {
 
+// ABOUT
+$scope.showAbout = true;
 
-  $scope.show = 1;
+$scope.aboutButton = function() {
+  hideEverything();
+  $scope.showAbout = true;
+}
+
+// HISTORY
+$scope.showHistory = false;
+
+$scope.historyButton = function() {
+  hideEverything();
+  $scope.showHistory = true;
+}
+
+//LEADERSHIP
+$scope.showLeadership = false;
+
+$scope.leadershipButton = function() {
+  hideEverything();
+  $scope.showLeadership = true;
+}
+
+// TESTIMONIAL
+$scope.showTestimonial = false;
+
+$scope.testimonialButton = function() {
+  hideEverything();
+  $scope.showTestimonial = true;
+}
+
+// FAQ
+$scope.showFaq = false;
+
+$scope.faqButton = function() {
+  hideEverything();
+  $scope.showFaq = true;
+
+}
+
+function hideEverything() {
+  $scope.showAbout = false;
+  $scope.showHistory = false;
+  $scope.showLeadership = false;
+  $scope.showTestimonial = false;
+  $scope.showFaq = false;
+}
+
+$scope.show = 1;
+
+
+
+
+
+
 
 });
 })();
