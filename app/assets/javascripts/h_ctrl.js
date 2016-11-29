49656bf9f5443e105784cf4afaddea64cb8d5179
David Basaraba <d.basaraba@gmail.com>
2016-11-28 20:57:06 -0600

    Changing names of controllers in angular

--- app/assets/javascripts/h_ctrl.js
+++ app/assets/javascripts/h_ctrl.js
@@ -1,7 +1,7 @@
 (function() {
   "use strict";
 
-  angular.module("app").controller("heroCtrl", function($scope) {
+  angular.module("app").controller("hCtrl", function($scope) {
 
 // ABOUT
 $scope.showAbout = true;
