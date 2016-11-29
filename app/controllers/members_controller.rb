4b9dea8d9b92180addba3f7c53599affadf1e87e
AriSendic <arimsendic@gmail.com>
2016-11-28 19:52:45 -0600

    added donation to member form and view

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,12 +5,13 @@ class MembersController < ApplicationController
   end
 
   def new
-  	render 'new.html.erb'
+  	@token = Braintree::ClientToken.generate
+    render 'new.html.erb'
   end
 
   def create
-  	member = Member.create(
-  		title: params[:title],
+    member = Member.create(
+    	title: params[:title],
       first_name: params[:first_name],
       middle_name: params[:middle_name],
       last_name: params[:last_name],
@@ -23,6 +24,42 @@ class MembersController < ApplicationController
       email: params[:email],
       phone_number: params[:phone_number]
   		)
-  	redirect_to '/members'
+
+  	nonce_from_the_client = params[:payment_method_nonce]
+    result = Braintree::Transaction.sale(
+      amount: params[:amount],
+      payment_method_nonce: nonce_from_the_client,
+      options: {
+        submit_for_settlement: true
+      }
+    )
+    if result.success?
+      puts 'success!: #{result.transaction.id}'
+      @donation = Donation.create(
+        title: params[:title],
+        first_name: params[:first_name],
+        middle_name: params[:middle_name],
+        last_name: params[:last_name],
+        suffix: params[:suffix],
+        address: params[:address],
+        city: params[:city],
+        state: params[:state],
+        zip_code: params[:zip_code],
+        country: params[:country],
+        email: params[:email],
+        phone_number: params[:phone],
+        amount: params[:amount],
+        bt_transaction_id: result.transaction.id
+      )
+      if @donation.save
+        redirect_to "/donations/#{@donation.id}"
+      else
+        render 'new.html.erb'
+      end
+    elsif result.transaction
+      puts 'Error processing transaction:'
+      puts '  code: #{result.transaction.processor_response_code}'
+      puts '  text: #{result.transaction.processor_response_text}'
+    end
   end
 end
