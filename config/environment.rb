d69b711c2333de6c6ed46f2fc86b70ba1d3fc19b
AriSendic <arimsendic@gmail.com>
2016-12-08 20:49:39 -0600

    designed email to send on event creation, defined @event in event notifier class in mailers

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => '100clubchicago.org',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
