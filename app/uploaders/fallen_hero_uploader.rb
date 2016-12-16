ee455778ab919b2f8b3d97f15c0a3036650ef646
David Basaraba <d.basaraba@gmail.com>
2016-12-15 20:52:22 -0600

    Carrierwave for fallen heroes is working

--- app/uploaders/fallen_hero_uploader.rb
+++ app/uploaders/fallen_hero_uploader.rb
@@ -4,7 +4,7 @@ class FallenHeroUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -32,15 +32,23 @@ class FallenHeroUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [100, 100]
+  end
+
+  version :medium do
+    process :resize_to_fit => [250, 250]
+  end
+
+  version :large do
+    process :resize_to_fit => [400, 400]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
