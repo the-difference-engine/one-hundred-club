1983b8f4f46347a07e683e7354896c32ac3fdb39
David Basaraba <d.basaraba@gmail.com>
2017-01-19 20:37:15 -0600

    Members index is set up with a link that points to each individual member show page. Capitalized member levels in member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,16 +4,16 @@ class Member < ActiveRecord::Base
   def check_amount(amount)
     amount = amount.to_i
     if amount.between?(50, 199)
-      self.level = 'bronze'
+      self.level = 'Bronze'
       return true
     elsif amount.between?(200, 499)
-      self.level = 'silver'
+      self.level = 'Silver'
       return true
     elsif amount.between?(500, 4999)
-      self.level = 'gold'
+      self.level = 'Gold'
       return true
     elsif amount > 5000
-      self.level = 'lifetime'
+      self.level = 'Lifetime'
       return true
     else
       return false
